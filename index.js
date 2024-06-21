ccac5113c7ed5e273de9cf98b46e2ed6096e6526
Anton Snizhko <mail.anton88@gmail.com>
2024-06-21 19:55:38 +0300

    chore: Update gitignore and remove .vercel from ignored files

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
+import { config } from 'dotenv'
+config()
+
+import { createServer } from 'http'
 import { ethers } from 'ethers'
 import { Bot } from 'grammy'
 
-// Замените 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота
-const bot = new Bot('7230110914:AAESDlirwRS4giORtdpeYUk0p0NixdVGZwA')
+const PORT = process.env.PORT || 3000
 
-// Список отслеживаемых кошельков и их текущие балансы
-let trackedWallets = {}
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN)
 
-// Интервал проверки в миллисекундах (например, каждые 1 сек.)
-const CHECK_INTERVAL = 1000
-
-// Функция для проверки балансов кошельков
-async function checkBalances() {
-  const provider = ethers.getDefaultProvider('mainnet')
-  for (const [address, oldBalance] of Object.entries(trackedWallets)) {
-    const newBalance = await provider.getBalance(address)
-    const formattedNewBalance = ethers.utils.formatEther(newBalance)
-
-    if (formattedNewBalance !== oldBalance) {
-      bot.api.sendMessage(
-        'YOUR_CHAT_ID', // Замените на ваш Telegram chat ID
-        `Баланс кошелька ${address} изменился: ${formattedNewBalance} ETH`
-      )
-      // Обновляем баланс в отслеживаемых кошельках
-      trackedWallets[address] = formattedNewBalance
-    }
-  }
-}
-
-// Запускаем периодическую проверку балансов
-setInterval(checkBalances, CHECK_INTERVAL)
-
-// Обработка команды /start
 bot.command('start', ctx => {
   ctx.reply(
     'Добро пожаловать! Используйте команду /addwallet <адрес> для отслеживания нового кошелька.'
   )
+  console.log('Received /start command')
 })
 
-// Обработка команды /addwallet
 bot.command('addwallet', async ctx => {
   const address = ctx.message.text.split(' ')[1]
   if (!address) {
@@ -49,12 +26,21 @@ bot.command('addwallet', async ctx => {
   const balance = await provider.getBalance(address)
   const formattedBalance = ethers.utils.formatEther(balance)
 
-  // Добавляем кошелек в список отслеживаемых
-  trackedWallets[address] = formattedBalance
   ctx.reply(
     `Кошелек ${address} добавлен для отслеживания. Текущий баланс: ${formattedBalance} ETH`
   )
+  console.log('Received /addwallet command')
 })
 
-// Запуск бота
 bot.start()
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello, world!\n')
+})
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`)
+  console.log(`TELEGRAM_BOT_TOKEN: ${process.env.TELEGRAM_BOT_TOKEN}`)
+  console.log(`YOUR_CHAT_ID: ${process.env.YOUR_CHAT_ID}`)
+})
